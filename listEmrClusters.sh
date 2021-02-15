4c8a3bc3163e9b76288a9142df35b7af4eeaa3e0
Sufi Nawaz <sufinawaz@gmail.com>
2021-02-15 12:38:45 -0500

    Param improvements for emr listing script

--- listEmrClusters.sh
+++ listEmrClusters.sh
@@ -1,5 +1,4 @@
-
-#!/bin/bash
+#!/bin/zsh
 
 SCRIPTS_DIR="$(dirname "$0")"
 BCYAN="\033[1;36m" 
@@ -7,7 +6,7 @@ BRED="\033[1;31m"
 URED="\033[4;31m"
 CRESET="\033[0m"
 
-if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [[ -z $1 ]]; then
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]] || [[ -z $1 ]]; then
     echo "Find EMR clusters with specified prefix [startDate,state]"
     echo -e "${BCYAN}Usage:${CRESET} $(basename $0) ${BRED}prefix${CRESET} ${BCYAN}[state] [startDate]${CRESET}"
     exit 0
@@ -15,19 +14,26 @@ fi
 
 Prefix=$1
 state=$2
-startDate=$3
+region=$3
+startDate=$4
 
 
 if [[ -z $state ]]; then
     state=WAITING
 fi
 
+if [[ -z $region ]]; then
+    region=us-east-1
+fi
+
 if [[ -z $startDate ]]; then
     startDate=$(date -v-3d '+%Y-%m-%d')
 fi
 
+echo PREFIX: $Prefix \| STATE: ${state} \| REGION: $region \| FROM: $startDate
+
 aws emr list-clusters \
-    --cluster-states $state \
+    --cluster-states ${state} --region $region \
     --query "Clusters[?starts_with(Name, \`$Prefix\`)==\`true\`]|[?Status.Timeline.CreationDateTime>=\`${startDate}\`]" \
     | \jq -r '.[]|"\(.Id),\(.Name),\(.Status.Timeline.CreationDateTime),\(.NormalizedInstanceHours)"' \
     | column -t -s,
